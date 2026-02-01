4ec0472b598774b995f780ac419ffa9ecd9f8ee5
Nick Klauer <klauer@gmail.com>
2026-02-01 17:49:06 -0600

    refactor(mulligan): extract card database to embedded JSON file
    Extract 428-line initializeCardDatabase function to JSON data file with
    go:embed. Reduces pkg/mulligan/cards.go from 446 lines to 54 lines.
    
    - Add pkg/mulligan/config/cards.json with card data
    - Add LoadCardDatabase() function using go:embed
    - Keep initializeCardDatabase() as deprecated wrapper for compatibility
    - Add CardConfig and CardInfoJSON types for JSON parsing
    
    Part of complexity reduction epic (clash-royale-api-gnih).
    Resolves clash-royale-api-2n7e.

--- pkg/mulligan/cards.go
+++ pkg/mulligan/cards.go
@@ -1,446 +1,53 @@
 package mulligan
 
-// initializeCardDatabase creates a database of cards with their mulligan properties
-func initializeCardDatabase() map[string]CardInfo {
-	return map[string]CardInfo{
-		// Win Conditions
-		"Hog Rider": {
-			Name:         "Hog Rider",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.8,
-		},
-		"Battle Ram": {
-			Name:         "Battle Ram",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.6,
-		},
-		"Goblin Barrel": {
-			Name:         "Goblin Barrel",
-			Elixir:       3,
-			Type:         "spell",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.2, // Never open with barrel
-		},
-		"Mortar": {
-			Name:         "Mortar",
-			Elixir:       4,
-			Type:         "building",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.1, // Never open with siege
-		},
-		"X-Bow": {
-			Name:         "X-Bow",
-			Elixir:       6,
-			Type:         "building",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.1,
-		},
-		"Rocket": {
-			Name:         "Rocket",
-			Elixir:       6,
-			Type:         "spell",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.1,
-		},
-		"Lightning": {
-			Name:         "Lightning",
-			Elixir:       6,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.2,
-		},
-		"Fireball": {
-			Name:         "Fireball",
-			Elixir:       4,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.3,
-		},
-		"Poison": {
-			Name:         "Poison",
-			Elixir:       4,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.3,
-		},
+import (
+	_ "embed"
+	"encoding/json"
+)
 
-		// Big Tanks (Beatdown)
-		"Golem": {
-			Name:         "Golem",
-			Elixir:       8,
-			Type:         "troop",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.0,
-		},
-		"Giant": {
-			Name:         "Giant",
-			Elixir:       5,
-			Type:         "troop",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.0,
-		},
-		"Lava Hound": {
-			Name:         "Lava Hound",
-			Elixir:       7,
-			Type:         "troop",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.0,
-		},
-		"Royal Giant": {
-			Name:         "Royal Giant",
-			Elixir:       6,
-			Type:         "troop",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.1,
-		},
-		"Baby Dragon": {
-			Name:         "Baby Dragon",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.6,
-		},
-		"Skeleton Dragons": {
-			Name:         "Skeleton Dragons",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.7,
-		},
+//go:embed config/cards.json
+var defaultCardsJSON []byte
 
-		// Defensive Buildings
-		"Cannon": {
-			Name:         "Cannon",
-			Elixir:       3,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.4, // Only reactive
-		},
-		"Tesla": {
-			Name:         "Tesla",
-			Elixir:       4,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.4,
-		},
-		"Inferno Tower": {
-			Name:         "Inferno Tower",
-			Elixir:       5,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.3,
-		},
-		"Bomb Tower": {
-			Name:         "Bomb Tower",
-			Elixir:       4,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.4,
-		},
-		"Furnace": {
-			Name:         "Furnace",
-			Elixir:       4,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.5,
-		},
-		"Goblin Cage": {
-			Name:         "Goblin Cage",
-			Elixir:       3,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.6,
-		},
-		"Tombstone": {
-			Name:         "Tombstone",
-			Elixir:       3,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.6,
-		},
+// CardConfig represents the JSON configuration structure for card data
+type CardConfig struct {
+	Version int                    `json:"version"`
+	Cards   map[string]CardInfoJSON `json:"cards"`
+}
 
-		// Spawn Buildings
-		"Goblin Hut": {
-			Name:         "Goblin Hut",
-			Elixir:       5,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.5,
-		},
-		"Barbarian Hut": {
-			Name:         "Barbarian Hut",
-			Elixir:       7,
-			Type:         "building",
-			Role:         RoleDefensive,
-			OpeningScore: 0.3,
-		},
-		"Elixir Collector": {
-			Name:         "Elixir Collector",
-			Elixir:       6,
-			Type:         "building",
-			Role:         RoleSupport,
-			OpeningScore: 0.7,
-		},
+// CardInfoJSON represents a card in the JSON configuration
+type CardInfoJSON struct {
+	Name         string  `json:"name"`
+	Elixir       int     `json:"elixir"`
+	Type         string  `json:"type"`
+	Rarity       string  `json:"rarity,omitempty"`
+	Role         string  `json:"role"`
+	OpeningScore float64 `json:"opening_score"`
+}
 
-		// Cycle Cards (Low Elixir)
-		"Skeletons": {
-			Name:         "Skeletons",
-			Elixir:       1,
-			Type:         "troop",
-			Role:         RoleCycle,
-			OpeningScore: 0.9,
-		},
-		"Ice Spirit": {
-			Name:         "Ice Spirit",
-			Elixir:       1,
-			Type:         "troop",
-			Role:         RoleCycle,
-			OpeningScore: 0.9,
-		},
-		"Fire Spirit": {
-			Name:         "Fire Spirit",
-			Elixir:       1,
-			Type:         "troop",
-			Role:         RoleCycle,
-			OpeningScore: 0.9,
-		},
-		"Zap": {
-			Name:         "Zap",
-			Elixir:       2,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.7,
-		},
-		"Log": {
-			Name:         "The Log",
-			Elixir:       2,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.7,
-		},
-		"Snowball": {
-			Name:         "Snowball",
-			Elixir:       2,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.7,
-		},
-		"Arrows": {
-			Name:         "Arrows",
-			Elixir:       3,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.6,
-		},
-
-		// Small Troops
-		"Goblins": {
-			Name:         "Goblins",
-			Elixir:       2,
-			Type:         "troop",
-			Role:         RoleCycle,
-			OpeningScore: 0.8,
-		},
-		"Bats": {
-			Name:         "Bats",
-			Elixir:       2,
-			Type:         "troop",
-			Role:         RoleCycle,
-			OpeningScore: 0.8,
-		},
-		"Minions": {
-			Name:         "Minions",
-			Elixir:       3,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.7,
-		},
-		"Minion Horde": {
-			Name:         "Minion Horde",
-			Elixir:       5,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.4,
-		},
-		"Goblin Gang": {
-			Name:         "Goblin Gang",
-			Elixir:       3,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.7,
-		},
-		"Archers": {
-			Name:         "Archers",
-			Elixir:       3,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.8,
-		},
-		"Knight": {
-			Name:         "Knight",
-			Elixir:       3,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.8,
-		},
-		"Bandit": {
-			Name:         "Bandit",
-			Elixir:       3,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.7,
-		},
-		"Wall Breakers": {
-			Name:         "Wall Breakers",
-			Elixir:       2,
-			Type:         "troop",
-			Role:         RoleCycle,
-			OpeningScore: 0.6,
-		},
-
-		// Medium Troops
-		"Valkyrie": {
-			Name:         "Valkyrie",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.6,
-		},
-		"Hunter": {
-			Name:         "Hunter",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.6,
-		},
-		"Musketeer": {
-			Name:         "Musketeer",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.5,
-		},
-		"Electro Wizard": {
-			Name:         "Electro Wizard",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.6,
-		},
-		"Dark Prince": {
-			Name:         "Dark Prince",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.6,
-		},
-		"Princess": {
-			Name:         "Princess",
-			Elixir:       3,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.5,
-		},
-
-		// Big Troops
-		"Barbarians": {
-			Name:         "Barbarians",
-			Elixir:       5,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.4,
-		},
-		"PEKKA": {
-			Name:         "PEKKA",
-			Elixir:       7,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.2,
-		},
-		"Bowler": {
-			Name:         "Bowler",
-			Elixir:       5,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.4,
-		},
-		"Executioner": {
-			Name:         "Executioner",
-			Elixir:       5,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.4,
-		},
-		"Witch": {
-			Name:         "Witch",
-			Elixir:       5,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.4,
-		},
-		"Lumberjack": {
-			Name:         "Lumberjack",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.6,
-		},
-
-		// Spells
-		"Rage": {
-			Name:         "Rage",
-			Elixir:       2,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.3,
-		},
-		"Freeze": {
-			Name:         "Freeze",
-			Elixir:       4,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.3,
-		},
-		"Giant Snowball": {
-			Name:         "Giant Snowball",
-			Elixir:       2,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.6,
-		},
-		"Barbarian Barrel": {
-			Name:         "Barbarian Barrel",
-			Elixir:       2,
-			Type:         "spell",
-			Role:         RoleSpell,
-			OpeningScore: 0.6,
-		},
-
-		// Champion Cards
-		"Phoenix": {
-			Name:         "Phoenix",
-			Elixir:       7,
-			Type:         "troop",
-			Role:         RoleWinCondition,
-			OpeningScore: 0.2,
-		},
-		"Monk": {
-			Name:         "Monk",
-			Elixir:       5,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.4,
-		},
-		"Architect": {
-			Name:         "Architect",
-			Elixir:       4,
-			Type:         "troop",
-			Role:         RoleSupport,
-			OpeningScore: 0.5,
-		},
+// LoadCardDatabase loads card data from embedded JSON
+func LoadCardDatabase() map[string]CardInfo {
+	var config CardConfig
+	if err := json.Unmarshal(defaultCardsJSON, &config); err != nil {
+		// Return empty map on error (should not happen with embedded data)
+		return make(map[string]CardInfo)
 	}
+
+	cards := make(map[string]CardInfo, len(config.Cards))
+	for name, cardJSON := range config.Cards {
+		cards[name] = CardInfo{
+			Name:         cardJSON.Name,
+			Elixir:       cardJSON.Elixir,
+			Type:         cardJSON.Type,
+			Rarity:       cardJSON.Rarity,
+			Role:         CardRole(cardJSON.Role),
+			OpeningScore: cardJSON.OpeningScore,
+		}
+	}
+	return cards
+}
+
+// Deprecated: initializeCardDatabase creates a database of cards with their mulligan properties
+// Use LoadCardDatabase instead. This function is kept for backward compatibility.
+func initializeCardDatabase() map[string]CardInfo {
+	return LoadCardDatabase()
 }
